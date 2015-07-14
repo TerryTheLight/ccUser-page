490c571dc0607233d9df3a479aebddc6619c5c91
JiaJing <richmanxinbox@gmail.com>
2015-07-14 18:20:28 +0800

    super responsive

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'user#index'
 
+   get '/forgot' => 'user#forgot'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
